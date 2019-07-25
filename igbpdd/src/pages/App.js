cb01b655685861689ef5464e8f3804cf7da515b3
Camilo Contreras <camilo.coo250@gmail.com>
2019-07-25 22:43:11 +0000

    preventfull commit for react socket library

--- igbpdd/src/pages/App.js
+++ igbpdd/src/pages/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
+let backgroundImage = {
+  width: "100%",
+  height: "auto",
+  backgroundImage: `url(${"http://www.iglesiabautistapoderdedios.com/prueba/wp-content/uploads/2017/05/Iglesia-Poder-de-Dios.jpg"})`,
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center center",
+  backgroundSize: "cover"
 
+};
+
+let blankBox={
+    heigth:"400px"
+}
 
 function Home() {
   return (
     <React.Fragment>
-        Hello from home
+        <div class="jumbotron p-0">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col"  style={backgroundImage}>
+                        <div className="text-light display-4">
+                            Bienvenidos
+                        </div>
+                        <div className="blanck box text-transparent" style={blankBox}>sadf</div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </React.Fragment>
   );
 }
