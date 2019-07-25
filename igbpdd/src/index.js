cc9dce85e39b23d34c4734993bf0f13b84bf07f7
Camilo Contreras <camilo.coo250@gmail.com>
2019-07-25 20:55:34 +0000

    navbar finished

--- igbpdd/src/index.js
+++ igbpdd/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import Home from './pages/App';
+import Navbar from './components/navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const routing = (
+<React.Fragment>
+    <Navbar/>
+    <Router>
+        <div>
+        <Route path="/" component={Home} />
+        </div>
+    </Router>
+  </React.Fragment>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ igbpdd/src/pages/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function Home() {
+  return (
+    <React.Fragment>
+        Hello from home
+    </React.Fragment>
+  );
+}
+
+export default Home;
